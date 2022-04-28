f65e0e23555990342792e4003b967ebdac12b9d2
hasnahenashimu <74762569+hasnahenashimu@users.noreply.github.com>
2022-04-29 01:45:53 +0600

    Initial Commit

---
+++ profileWorkings/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from profileWorkings.models import *
+
+# Register your models here.
+
+admin.site.register(UserInformations)
+admin.site.register(UserLinks)
+admin.site.register(Hide)
---
+++ profileWorkings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProfileworkingsConfig(AppConfig):
+    name = 'profileWorkings'
